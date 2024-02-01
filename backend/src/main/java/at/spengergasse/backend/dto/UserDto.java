935a98c99845366925e5ef694544e8d92517fc33
wac22585 <wac22585@spengergasse.at>
2024-02-01 09:51:38 +0100

    Added password attribute to enable password changes from the frontend. The fromEntity method will be used in the service class to convert an entity to a dto.

--- backend/src/main/java/at/spengergasse/backend/dto/UserDto.java
+++ backend/src/main/java/at/spengergasse/backend/dto/UserDto.java
@@ -1,5 +1,7 @@
 package at.spengergasse.backend.dto;
 
+import at.spengergasse.backend.model.User;
+
 import java.sql.Date;
 import java.util.List;
 import java.util.UUID;
@@ -9,9 +11,25 @@ public record UserDto (
         String firstname,
         String lastname,
         String email,
+        String password,
         Boolean deleted,
         Date created,
         Date deletedDate,
         List<String> roles
         )
-{ }
+{
+        //This method is used to convert an entity to a dto for the service layer
+        public static UserDto fromEntity(User user) {
+                return new UserDto(
+                        user.getId(),
+                        user.getFirstname(),
+                        user.getLastname(),
+                        user.getEmail(),
+                        user.getPassword(),
+                        user.getDeleted(),
+                        user.getCreated(),
+                        user.getDeletedDate(),
+                        user.getRoles()
+                );
+        }
+}
