cfc6e557acb791fb1e096e199cacb57b6d27b5e1
szh22482 <122881874+szh22482@users.noreply.github.com>
2024-02-08 01:32:47 +0100

    Added table with headers, styling missing

--- frontend/src/views/Home.vue
+++ frontend/src/views/Home.vue
@@ -1,6 +1,50 @@
 <template>
-  <h1>Usermanagement</h1>
+  <v-data-table
+    :items="users"
+    :headers="headers"
+    >
+    <template v-slot:item.profilepicture="{ item }">
+      <profile-picture
+        :firstname="item.firstname"
+        :lastname="item.lastname"
+        :color-nr="item.colorNr"
+      />
+    </template>
+  </v-data-table>
 </template>
 
-<script setup>
+<script>
+import axios from "axios";
+import ProfilePicture from "@/components/ProfilePicture.vue";
+export default {
+  components: {
+    ProfilePicture
+  },
+  data() {
+    return {
+      headers: [
+        {title: "Profile Picture", key: "profilepicture"},
+        {title: "Firstname", key: "firstname"},
+        {title: "Lastname", key: "lastname"},
+        {title: "Email", key: "email" },
+        {title: "Created", key: "created"},
+        {title: "Roles", key: "roles"}
+      ],
+      users: [],
+    }
+  },
+  async mounted() {
+    try {
+      const response = await axios.get("/users/all");
+      if(response != null) {
+        this.users = response.data;
+      }
+      else {
+        alert("No data found");
+      }
+    } catch (e) {
+      alert(e)
+    }
+  }
+}
 </script>
